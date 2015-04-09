93a8dc4dc76f5faae526ae0fac8b0127c6865aa1
Rajendra Gadhi <rajungandhi@yahoo.com>
2015-04-09 09:31:26 +0530

    Added class files

---
+++ TrialDemo/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrialDemo
+{
+    class Login
+    {
+        public Login()
+        {
+                //login constructor
+        }
+    }
+}
